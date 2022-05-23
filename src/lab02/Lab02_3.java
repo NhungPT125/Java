b54d4332700215f337e2809a28f76f160084af98
unknown <nhungptt.fingroup@gmail.com>
2022-05-23 23:42:00 +0700

    update lab02_3

--- src/lab02/Lab02_3.java
+++ src/lab02/Lab02_3.java
@@ -11,20 +11,24 @@ public class Lab02_3 {
         System.out.println("Please enter your height (m): ");
         float inputHeight = scanner.nextFloat();
 
-        Float BMI = inputWeight / (inputHeight * 2);
+        float BMI = inputWeight / (inputHeight * 2);
 
         System.out.printf("Your weight is: %.2f\n ", inputWeight);
         System.out.printf("Your height is: %.2f\n ",inputHeight);
         System.out.printf("Your BMI is: %.2f\n ", BMI);
 
-        if(BMI <= 18.5){
+
+
+
+       if(BMI <= 18.5){
+           double normalWeight = 18.5 * (inputHeight * 2);
             System.out.println("Underweight");
-        }else if(BMI <= 24.9){
-            System.out.println("Normal weight");
-        }else if(BMI >= 25 && BMI <=29.9){
+            System.out.println("You should increase:"+ (normalWeight - inputWeight));
+        }else if(BMI > 24.9){
+           double normalWeight = 24.9 * (inputHeight * 2);
             System.out.println("Overweight");
-        }else {
-            System.out.println("Obesity");
+            System.out.println("You should decrease:" + (inputWeight - normalWeight));
+
         }
 
     }
